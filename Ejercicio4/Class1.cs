df507f04d50d8183b72d4376d7849fdaf5894a34
edunor2804 <134438869+edunor2804@users.noreply.github.com>
2026-06-28 23:42:31 -0500

    Se añadio el Ejercicio 4

---
+++ Ejercicio4/Class1.cs
@@ -0,0 +1,26 @@
+﻿public class Class1
+{
+    public static void encabezado()
+    {
+        Console.WriteLine("╔═════════════════════════════════════╗");
+
+        Console.WriteLine("║         Calcular mi Peso             ║");
+
+        Console.WriteLine("╚═════════════════════════════════════╝");
+    }
+    public static class CalcularPeso
+    {
+        // MÉTODO POR VALOR
+        public static double CalcularIMC(double peso, double altura)
+        {
+            return peso / (altura * altura); // Fórmula del IMC
+        }
+
+        // MÉTODO POR REFERENCIA
+        public static void AplicarPlanGimnasio(ref double pesoActual, double kilosPerdidos)
+        {
+            pesoActual = pesoActual - kilosPerdidos; // Altera el peso declarado inciialmente
+        }
+    }
+}
+
---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Class1;
+
+namespace Otro
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            encabezado();
+            // Variables Iniciales
+            double peso = 80.0;
+            double altura = 1.75;
+
+            Console.WriteLine($"--- DATOS INICIALES ---");
+            Console.WriteLine($"Peso: {peso} kg");
+            Console.WriteLine($"Altura: {altura} m\n");
+
+            // IMPRIMIMOS EL IMC BASANDONOS EN LOS VALORES INICIALES
+            double imc = CalcularPeso.CalcularIMC(peso, altura);
+            Console.WriteLine($"Tu IMC actual es: {imc:F2}\n"); //26.12
+
+            // POR REFERENCIA
+            // Usamos ref para simular que la persona bajó 4 kilos en el gimnasio
+            Console.WriteLine("Pasaron 2 meses de gimnasio y la persona perdió 4 kg...\n");
+            CalcularPeso.AplicarPlanGimnasio(ref peso, 4.0);
+
+            // El peso original cambió de forma permanente
+            Console.WriteLine($"Ahora el peso es de {peso} kg");
+
+            // Calculamos el nuevo IMC con el peso actualizado
+            double nuevoImc = CalcularPeso.CalcularIMC(peso, altura);
+            Console.WriteLine($"Tu nuevo IMC es: {nuevoImc:F2}");
+        }
+    }
+
+}
